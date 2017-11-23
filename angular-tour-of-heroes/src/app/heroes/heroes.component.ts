3c567c830e4eda84c0b8dfde529a23b56d618508
Yoann Gauthier <yoann.gauthier@thalesgroup.com>
2017-11-23 16:24:46 +0100

    Display hero details

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    console.log("HeroesComponent:onSelect " + hero.name);
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
--- angular-tour-of-heroes/src/app/heroes/heroes.module.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
@@ -9,6 +10,7 @@ import { HeroesComponent } from './heroes.component';
 
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     TranslateModule,
     FlexLayoutModule,
