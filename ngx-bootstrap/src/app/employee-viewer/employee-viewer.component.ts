55b754d36d801bd895cea638b5f71bf1a2a767b5
Yoann Gauthier <yoann.gauthier@thalesgroup.com>
2017-12-01 17:27:33 +0100

    Add alert for backend fail update

--- ngx-bootstrap/src/app/employee-viewer/employee-viewer.component.ts
+++ ngx-bootstrap/src/app/employee-viewer/employee-viewer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { debounceTime } from 'rxjs/operator/debounceTime';
 import { EmployeeService, Employee } from './employee.service'
 
 @Component({
@@ -11,13 +13,30 @@ export class EmployeeViewerComponent implements OnInit {
   selected_employee: Employee;
   employee_list: Employee[];
   display_edition: Boolean = false;
+
+
+  private _success = new Subject<string>();
+  errorMessage: string;
+
+
   constructor(private employeeService: EmployeeService) { }
 
   /**
    * Ng On Init call after construction
    */
-  ngOnInit() {
+  ngOnInit(): void {
     this.fill_employee_list();
+
+    this._success.subscribe((message) => this.errorMessage = message);
+    debounceTime.call(this._success, 5000).subscribe(() => {this.errorMessage = null});
+  }
+
+  /**
+   * Change error message
+   */
+  private changeErrorMessage(updatedEmployee: Employee):void {
+    console.log("EmployeeViewerComponent:changeErrorMessage");
+    this._success.next("Unable to udpate employee, connection error...." + JSON.stringify(updatedEmployee));
   }
 
   /**
@@ -34,7 +53,7 @@ export class EmployeeViewerComponent implements OnInit {
    * @param employee selected employee
    */
   public onSelect(employee: Employee): void {
-    //this.selected_employee = employee;
+    // On copie l'object pour ne pas modifier la liste
     this.selected_employee = Object.assign({}, employee);
     this.display_edition = true;
   }
@@ -44,8 +63,13 @@ export class EmployeeViewerComponent implements OnInit {
    * @param updatedEmployee Updated employee
    */
   private updateItem(updatedEmployee: Employee): void {
-    var foundIndex = this.employee_list.findIndex(x => x.id == updatedEmployee.id);
-    this.employee_list[foundIndex] = updatedEmployee;
+    if (updatedEmployee) { // si non undefined, undefined == erreur http
+      var foundIndex = this.employee_list.findIndex(x => x.id == updatedEmployee.id);
+      this.employee_list[foundIndex] = updatedEmployee;
+    }
+    else {
+      this.changeErrorMessage(this.selected_employee);
+    }
   }
 
   /**
@@ -54,10 +78,9 @@ export class EmployeeViewerComponent implements OnInit {
   public edit(): void {
     if (this.selected_employee) {
       // Update
-      console.log("New Name " + this.selected_employee.first_name);
       this.display_edition = false;
       this.employeeService.updateEmployees(this.selected_employee).subscribe(
-        _ => this.updateItem(this.selected_employee)
+        result => this.updateItem(result)
       )
     }
   }
--- ngx-bootstrap/src/app/employee-viewer/employee-viewer.module.ts
+++ ngx-bootstrap/src/app/employee-viewer/employee-viewer.module.ts
@@ -4,11 +4,13 @@ import { EmployeeViewerComponent } from './employee-viewer.component'
 import { EmployeeViewerRoutingModule } from './employee-viewer-routing.module'
 import { EmployeeService } from "./employee.service";
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    NgbModule,
     EmployeeViewerRoutingModule
   ],
   declarations: [EmployeeViewerComponent],
